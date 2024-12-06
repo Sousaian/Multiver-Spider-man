403c168f09ae707f56ab96b3e9c7ef7de539ada7
Ian Alves Sousa <iansousa.dev@gmail.com>
2024-12-06 11:12:34 -0300

    Better compatibility for mobile in home page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,14 +23,29 @@ document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
 function selectCarouselItem(selectedButtonElement) {
   const selectedItem = selectedButtonElement.id;
   const carousel = document.querySelector('.s-cards-carousel');
-  const transform = carousel.style.transform;
-  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+  const cards = document.querySelectorAll('.s-card');
+  const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
   const rotateYDeg = -120 * (Number(selectedItem) - 1);
-  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+  carousel.style.transform = `translateZ(-40vw) rotateY(${rotateYDeg}deg)`;
 
-  carousel.style.transform = newTransform;
-
+  // Atualiza os botões ativos
   const activeButtonElement = document.querySelector('.s-controller__button--active');
   activeButtonElement.classList.remove('s-controller__button--active');
   selectedButtonElement.classList.add('s-controller__button--active');
+
+  
+  // Alterna entre cartões
+  cards.forEach((card, index) => {
+    card.classList.remove('s-card--active');
+    if (index === Number(selectedItem) - 1) {
+      card.classList.add('s-card--active');
+    }
+    // Para mobile, esconde completamente os cartões inativos após a transição
+    if (isMobile) {
+      setTimeout(() => {
+        card.style.display = index === Number(selectedItem) - 1 ? "block" : "none";
+      }, 400); // Igual ao tempo da transição CSS
+    }
+  });
 }
